8a4567e3ecc0563dd4c13b6d40cf840915ef2167
tishkunov <generali8@mail.ru>
2018-05-24 13:55:32 +0200

    fix actions

--- src/actions/fetchData.js
+++ src/actions/fetchData.js
@@ -1,3 +1,5 @@
+import * as types from './types';
+
 export const fetchData = () => dispatch => {
 	fetch("/data.json", {
 	 	headers : {
@@ -8,7 +10,7 @@ export const fetchData = () => dispatch => {
     .then(res => res.json())
     .then(json =>
       dispatch({
-        type: "FETCH_DATA",
+        type: types.FETCH_DATA,
         payload: json
       })
     );
@@ -16,28 +18,28 @@ export const fetchData = () => dispatch => {
 
 export const searchQuery = (event) => dispatch => {
 	return dispatch({
-		type: "SEARCH_QUERY",
+		type: types.SEARCH_QUERY,
 		payload:event.target.value
 	})
 };
 
 export const sortByAge = (event) => dispatch => {
 	return dispatch({
-		type: "SORT_AGE",
+		type: types.SORT_AGE,
 		payload:event.target.getAttribute('filter')
 	})
 };
 
 export const sortByName = (event) => dispatch => {
 	return dispatch({
-		type: "SORT_NAME",
+		type: types.SORT_NAME,
 		payload:event.target.getAttribute('filter')
 	})
 };
 
 export const choosePlayer = (event) => dispatch => {
 	return dispatch({
-		type:"CHOOSE_PLAYER",
+		type:types.CHOOSE_PLAYER,
 		payload:event.currentTarget.getAttribute('datakey')
 	})
 }
---
+++ src/actions/types.js
@@ -0,0 +1,5 @@
+export const SEARCH_QUERY = 'SEARCH_QUERY';
+export const SORT_AGE = 'SORT_AGE';
+export const FETCH_DATA = 'FETCH_DATA';
+export const SORT_NAME = 'SORT_NAME';
+export const CHOOSE_PLAYER = 'CHOOSE_PLAYER';
